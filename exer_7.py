cbeb67f7d95a50d98bda42930383f38214abb147
Zohaib Yunus <zohaib.yunus@gmail.com>
2017-04-04 21:07:21 -0700

    changes to exer 6 & 7

--- exer_7.py
+++ exer_7.py
@@ -3,6 +3,11 @@ import json
 from pprint import pprint as pp
 
 with open("exer6_yaml.yml") as f:
-    new_list = yaml.load(f)
+    new_yaml_list = yaml.load(f)
+with open("exer6_json.json") as f:
+    new_json_list = json.load(f)
 
-pp(new_list)
+print "YAML list \n"
+pp(new_yaml_list)
+print "\n JSON list"
+pp(new_json_list)
