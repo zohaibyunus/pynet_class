106a80bedeb9f861f7824b2bc36ce7f6d3e7cebd
Zohaib Yunus <zohaib.yunus@gmail.com>
2017-06-13 13:17:13 -0700

    exer2

---
+++ week4/exer2.py
@@ -0,0 +1,46 @@
+import paramiko
+import time
+
+def new_connection(ip,user,passwd,port):
+    remote_conn_pre = paramiko.SSHClient()
+
+    ## Ignore host keys
+    remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    remote_conn_pre.connect(ip,username=user,password=passwd,port=port,look_for_keys=False,allow_agent=False)
+    remote_conn = remote_conn_pre.invoke_shell()
+    return remote_conn
+
+def disable_paging(remote_conn,cmd='terminal length 0'):
+
+	remote_conn.send(cmd+'\n')
+	time.sleep(1)
+
+def send_command(remote_conn,cmd):
+
+    remote_conn.send(cmd+"\n")
+    time.sleep(1)
+    if remote_conn.recv_ready():
+    	output = remote_conn.recv(5000)
+    else:
+	output = ''
+
+    print output
+
+def main():
+
+    ip = '184.105.247.71'
+    user = 'pyclass'
+    passwd = '88newclass'
+    port = 22
+    
+    conn = new_connection(ip,user,passwd,port)
+    disable_paging(conn)
+    send_command(conn,cmd='config t')
+    send_command(conn,cmd='logging buffered 10000')
+    send_command(conn,cmd='end')
+
+if __name__ == '__main__':
+    main()
+
+
